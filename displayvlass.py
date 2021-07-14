658a3a011f6d0002c368f9fa56746b6e0a07f5bb
brkent <65732670+brkent@users.noreply.github.com>
2021-07-14 12:40:20 -0400

    Create displayvlass.py
    First commit of VLASS image display script prototype.

---
+++ displayvlass.py
@@ -0,0 +1,23 @@
+from astropy.io import fits
+from astropy import wcs
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import aplpy
+
+target_url = 'http://www.cv.nrao.edu/~bkent/VLASS1.1.ql.T12t35.J231013+073000.10.2048.v1.I.iter1.image.pbcor.tt0.subim.fits'
+
+VLASS_FITS = fits.open(target_url)
+
+fig = plt.figure(facecolor='w', edgecolor='w', frameon=True, figsize=(10,9))
+
+ax = aplpy.FITSFigure(VLASS_FITS, figure=fig)
+
+ax.show_colorscale(cmap='gray', vmin=-.0001, vmax=0.00080)
+
+ax.add_grid()
+ax.grid.show()
+ax.set_title(os.path.basename(target_url))
+
+fig.canvas.draw()
